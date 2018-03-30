1d2bc7e005d3f949fb0ed068ad2e232c5085f004
Fabio Marques <140039082@aluno.unb.br>
2018-03-30 18:41:08 -0300

    Adding Input Manager.

---
+++ T2/include/InputManager.h
@@ -0,0 +1,14 @@
+//
+// Created by fabio on 30/03/18.
+//
+
+#ifndef T2_INPUTMANAGER_H
+#define T2_INPUTMANAGER_H
+
+
+class InputManager {
+
+};
+
+
+#endif //T2_INPUTMANAGER_H
---
+++ T2/src/InputManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by fabio on 30/03/18.
+//
+
+#include "InputManager.h"
